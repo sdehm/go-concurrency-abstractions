8fff20aa5611ef69540effb58997d2569395c803
Shaffer Dehmlow <sdehm@github.com>
2022-11-03 15:15:21 -0600

    Rename and format

--- events/events.go
+++ events/events.go
@@ -4,22 +4,23 @@ import "sync"
 
 type Publisher[T any] struct {
 	sync.Mutex
-	targets map[int]chan T
-	lastID  int
-	wg      sync.WaitGroup
+	subscribers map[int]chan T
+	lastID      int
+	wg          sync.WaitGroup
 }
 
 func NewPublisher[T any]() *Publisher[T] {
 	return &Publisher[T]{
-		targets: make(map[int]chan T),
+		subscribers: make(map[int]chan T),
 	}
 }
 
+// Publish a message to all subscribers.
 func (p *Publisher[T]) Publish(m T) {
 	p.Lock()
 	defer p.Unlock()
 
-	for _, c := range p.targets {
+	for _, c := range p.subscribers {
 		p.wg.Add(1)
 		go func(c chan T) {
 			defer p.wg.Done()
@@ -28,11 +29,12 @@ func (p *Publisher[T]) Publish(m T) {
 	}
 }
 
+// Close all subscriber channels
 func (p *Publisher[T]) Stop() {
 	p.Lock()
 	defer p.Unlock()
 
-	for _, c := range p.targets {
+	for _, c := range p.subscribers {
 		close(c)
 	}
 }
@@ -46,7 +48,7 @@ func (p *Publisher[T]) subscribe(s *Subscriber[T]) int {
 	defer p.Unlock()
 
 	id := p.lastID + 1
-	p.targets[id] = s.channel
+	p.subscribers[id] = s.channel
 	p.lastID = id
 	return id
 }
@@ -55,7 +57,7 @@ func (p *Publisher[T]) unsubscribe(id int) {
 	p.Lock()
 	defer p.Unlock()
 
-	delete(p.targets, id)
+	delete(p.subscribers, id)
 }
 
 type Subscriber[T any] struct {
